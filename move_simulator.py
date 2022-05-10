b89bae2475559bc6eed8c27a41859f01cfe1a83a
Xinyu Wu <59054117+walliampeace@users.noreply.github.com>
2022-05-10 02:47:40 -0400

    delete useless codes

--- move_simulator.py
+++ move_simulator.py
@@ -48,27 +48,6 @@ def movebase_client():
     move(client, -10, 8, 3.14)
     move(client, -12, 12, 1.57)
     move(client, -15, 15, 3.14)
-    # Creates a new goal with the MoveBaseGoal constructor
-    # goal = MoveBaseGoal()
-    # goal.target_pose.header.frame_id = "map"
-    # goal.target_pose.header.stamp = rospy.Time.now()
-    # # Move 0.5 meters forward along the x axis of the "map" coordinate frame
-    # # goal.target_pose.pose.position.x = 0.5
-    # goal.target_pose.pose = Pose(Point(-6, 4, 0), Quaternion(0.0, 0.0, 0.686, 0.77))
-    # # No rotation of the mobile base frame w.r.t. map frame
-    # # goal.target_pose.pose.orientation.w = 1.0
-    #
-    # # Sends the goal to the action server.
-    # client.send_goal(goal)
-    # # Waits for the server to finish performing the action.
-    # wait = client.wait_for_result()
-    # # If the result doesn't arrive, assume the Server is not available
-    # if not wait:
-    #     rospy.logerr("Action server not available!")
-    #     rospy.signal_shutdown("Action server not available!")
-    # else:
-    #     # Result of executing the action
-    #     return client.get_result()
 
 
     # If the python node is executed as main process (sourced directly)
